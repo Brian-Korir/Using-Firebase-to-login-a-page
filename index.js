c09c280edc4895efb3ae0c206cea7bb75d3274a6
Brian Korir <144766290+Brian-Korir@users.noreply.github.com>
2024-05-28 21:11:01 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 /*===imports===*/
 import { initializeApp } from "firebase/app"
 /*===Firebase setup===*/
+const firebaseConfig = {
+  apiKey: "AIzaSyASJyunjd-33MLfjFHRMgCmr3TB8x5ebj8",
+  authDomain: "korir-12.firebaseapp.com",
+  projectId: "korir-12",
+  storageBucket: "korir-12.appspot.com",
+  messagingSenderId: "680093194041",
+  appId: "1:680093194041:web:102ee18ad1823a62877ae8",
+  measurementId: "G-GP5LL6T0S2"
+};
 
 
